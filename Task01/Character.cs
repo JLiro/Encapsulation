a7d32b11aecf0706583df77c8ddf5a562636d61b
JLiro <0jayliro0@gmail.com>
2022-12-20 22:26:55 +0300

    Добавьте файлы проекта.

---
+++ Task01/Character.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task01
+{
+    class Character
+    {
+        private string _name;
+        private int _horizontalPosition;
+        private int _verticalPosition;
+        private bool _isAlive;
+
+        private Random random = new Random();
+
+        public Character(string name, int horizontalPosition, int verticalPosition, bool isAlive)
+        {
+            _name = name;
+            _horizontalPosition = horizontalPosition;
+            _verticalPosition = verticalPosition;
+            _isAlive = isAlive;
+        }
+
+        public int HorizontalPosition => _horizontalPosition;
+        public int VerticalPosition => _verticalPosition;
+
+        public void Kill()
+        {
+            _isAlive = false;
+        }
+
+        public void Move()
+        {
+            if (_horizontalPosition == 0 && _horizontalPosition == 0) return;
+
+            _horizontalPosition += _horizontalPosition > 0 ? random.Next(-1, 1) : 0;
+            _verticalPosition += _verticalPosition > 0 ? random.Next(-1, 1) : 0;
+        }
+
+        public void Show(int numberCharacter)
+        {
+            if (_isAlive == false) return;
+            Console.SetCursorPosition(_horizontalPosition, _verticalPosition);
+            Console.Write($"{numberCharacter}");
+        }
+    }
+}
---
+++ Task01/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        private static Character[] characters =
+        {
+            new Character("Character01", 10, 10, true),
+            new Character("Character02", 15, 15, true),
+            new Character("Character03", 25, 25, true)
+        };
+
+        private static void CheckCharactersDeath()
+        {
+            for (int index = 0; index < characters.Length; index++)
+            {
+                for (int nextIndex = index + 1; nextIndex < characters.Length; nextIndex++)
+                {
+                    Character СurrentСharacter = characters[index];
+                    Character NextCharacter = characters[nextIndex];
+
+                    if (СurrentСharacter.HorizontalPosition == NextCharacter.HorizontalPosition &&
+                        СurrentСharacter.VerticalPosition == NextCharacter.VerticalPosition)
+                    {
+                        СurrentСharacter.Kill();
+                        NextCharacter.Kill();
+                    }
+                }
+            }
+        }
+
+        private static void MoveAllCharacters()
+        {
+            for (int index = 0; index < characters.Length; index++)
+            {
+                characters[index].Move();
+            }
+        }
+
+        private static void ShowAllCharacters()
+        {
+            for (int index = 0; index < characters.Length; index++)
+            {
+                characters[index].Show(index);
+            }
+        }
+
+        private static void Main()
+        {
+            while (true)
+            {
+                CheckCharactersDeath();
+                MoveAllCharacters();
+                ShowAllCharacters();
+            }
+        }
+    }
+}
